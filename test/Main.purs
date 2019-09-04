1f323a39518ef332ef6af780e1c94b58bc99e3ec
Nick Saunders <nick@saunde.rs>
2019-09-04 12:45:38 -0700

    Add parsing tests.

--- test/Main.purs
+++ test/Main.purs
@@ -2,6 +2,7 @@ module Test.Main where
 
 import Prelude
 import Data.Either (Either(..))
+import Data.Traversable (traverse_)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Foreign.Object (fromHomogeneous) as FO
@@ -10,7 +11,7 @@ import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 import Type.Data.Row (RProxy(..))
-import TypedEnv (Variable, EnvError(..), fromEnv)
+import TypedEnv (type (<:), EnvError(..), fromEnv)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] $
@@ -18,21 +19,54 @@ main = launchAff_ $ runSpec [consoleReporter] $
 
     it "reads the requested environment" do
       let
-        env = FO.fromHomogeneous { "FOO": "foo!", "COUNT": "5", "GREETING": "Hello Sailor!", "DEBUG": "1" }
-        expected = Right { count: 5, greeting: "Hello Sailor!" }
-        actual = fromEnv (RProxy :: RProxy (count :: Variable "COUNT" Int, greeting :: Variable "GREETING" String)) env
+        env = FO.fromHomogeneous { "A": "a value", "B": "b value", "C": "c value" }
+        expected = Right { b: "b value", c: "c value" }
+        actual = fromEnv (RProxy :: RProxy (b :: String <: "B", c :: String <: "C")) env
       actual `shouldEqual` expected
 
     it "indicates when a lookup has failed" do
       let
-        env = FO.fromHomogeneous { "MAX_USERS": "50" }
-        expected = Left (EnvLookupError "MAX_USER_COUNT")
-        actual = fromEnv (RProxy :: RProxy (maxUsers :: Variable "MAX_USER_COUNT" Int)) env
+        env = FO.fromHomogeneous { "GREETING": "Hello" }
+        expected = Left (EnvLookupError "MESSAGE")
+        actual = fromEnv (RProxy :: RProxy (message :: String <: "MESSAGE")) env
       actual `shouldEqual` expected
 
     it "indicates when parsing a value has failed" do
       let
         env = FO.fromHomogeneous { "DEBUG": "50" }
         expected = Left (EnvParseError "DEBUG")
-        actual = fromEnv (RProxy :: RProxy (debug :: Variable "DEBUG" Boolean)) env
+        actual = fromEnv (RProxy :: RProxy (debug :: Boolean <: "DEBUG")) env
+      actual `shouldEqual` expected
+
+    it "parses boolean values" do
+      traverse_
+        (\({ given, expected }) ->
+          shouldEqual
+            (fromEnv (RProxy :: RProxy (actual :: Boolean <: "A")) (FO.fromHomogeneous { "A": given }))
+            (Right { actual: expected }))
+        [ { given: "0", expected: false }
+        , { given: "false", expected: false }
+        , { given: "1", expected: true }
+        , { given: "true", expected: true }
+        ]
+
+    it "parses integer values" do
+      let
+        env = FO.fromHomogeneous { "VALUE": "123" }
+        expected = Right { value: 123 }
+        actual = fromEnv (RProxy :: RProxy (value :: Int <: "VALUE")) env
+      actual `shouldEqual` expected
+
+    it "parses character values" do
+      let
+        env = FO.fromHomogeneous { "VALUE": "x" }
+        expected = Right { value: 'x' }
+        actual = fromEnv (RProxy :: RProxy (value :: Char <: "VALUE")) env
+      actual `shouldEqual` expected
+
+    it "parses number values" do
+      let
+        env = FO.fromHomogeneous { "VALUE": "123.456" }
+        expected = Right { value: 123.456 }
+        actual = fromEnv (RProxy :: RProxy (value :: Number <: "VALUE")) env
       actual `shouldEqual` expected
