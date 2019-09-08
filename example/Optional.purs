dda60bb64f75543f013c95d8d69d43a9290d9efe
Nick Saunders <nick@saunde.rs>
2019-09-08 11:34:59 -0700

    Create Optional example.

--- example/Optional.purs
+++ example/Optional.purs
@@ -1,8 +1,8 @@
-module Example.Basic where
+module Example.Optional where
 
 import Prelude
 import Data.Either (Either(..))
-import Data.List.Lazy (replicateM)
+import Data.Maybe (Maybe, fromMaybe)
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Process (getEnv)
@@ -10,17 +10,14 @@ import Type.Data.Row (RProxy(..))
 import TypedEnv (type (<:), envErrorMessage)
 import TypedEnv (fromEnv) as TypedEnv
 
-type Environment =
-  ( greeting :: String <: "GREETING"
-  , count    :: Int    <: "COUNT"
-  )
+type Settings = ( username :: Maybe String <: "USERNAME" )
 
 main :: Effect Unit
 main = do
-  env <- TypedEnv.fromEnv (RProxy :: RProxy Environment) <$> getEnv
+  env <- TypedEnv.fromEnv (RProxy :: RProxy Settings) <$> getEnv
   case env of
     Left error ->
       log $ "ERROR: " <> envErrorMessage error
-    Right { greeting, count } -> do
-      _ <- replicateM count (log greeting)
+    Right { username } -> do
+      log $ "Hello, " <> fromMaybe "Sailor" username <> "!"
       pure unit
