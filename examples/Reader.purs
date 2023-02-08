27a81c5fdfc1f1bd3f8e47748d2422132ab09520
Nick Saunders <nick@saunde.rs>
2023-02-08 13:34:34 -0700

    Updates for PS 0.15 and related workflow improvements (#5)

--- examples/Reader.purs
+++ examples/Reader.purs
@@ -1,6 +1,7 @@
 module Example.Reader where
 
 import Prelude
+
 import Control.Monad.Reader (Reader, asks, runReader)
 import Data.Either (Either(..))
 import Data.List.Lazy (replicateM)
@@ -8,18 +9,18 @@ import Data.Maybe (Maybe, fromMaybe)
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Process (getEnv)
-import Type.Data.Row (RProxy(..))
+import Type.Proxy (Proxy(..))
 import TypedEnv (type (<:), envErrorMessage)
 import TypedEnv (fromEnv) as TypedEnv
 
 type Config =
   ( username :: Maybe String <: "USERNAME"
-  , repeat   :: Maybe Int    <: "REPEAT"
+  , repeat :: Maybe Int <: "REPEAT"
   )
 
 main :: Effect Unit
 main = do
-  env <- TypedEnv.fromEnv (RProxy :: RProxy Config) <$> getEnv
+  env <- TypedEnv.fromEnv (Proxy :: Proxy Config) <$> getEnv
   case env of
     Left error ->
       log $ "ERROR: " <> envErrorMessage error
@@ -28,4 +29,6 @@ main = do
       pure unit
 
 greeting :: forall r. Reader { username :: Maybe String | r } String
-greeting = asks _.username >>= \username -> pure $ "Hello, " <> fromMaybe "Sailor" username <> "!"
+greeting = asks _.username >>= \username -> pure $ "Hello, "
+  <> fromMaybe "Sailor" username
+  <> "!"
--- shell.nix
+++ shell.nix
@@ -1,21 +1,28 @@
-{ pkgs ? import <nixpkgs> {} }:
-
 let
-  easy-ps = import
-    (pkgs.fetchFromGitHub {
-      owner = "justinwoo";
-      repo = "easy-purescript-nix";
-      rev = "c766802fa7a6194fd80c31d8907f07ebacc4869c";
-      sha256 = "0f0b3bhf8id1vnga0vaj7bldfykis8vxfiyll3rc003b21sx9w4n";
-    }) {
-    inherit pkgs;
-  };
-in
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz";
+  }) {};
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.nodejs-14_x
-    easy-ps.purs-0_14_1
-    easy-ps.spago
-  ];
-}
+  # To update to a newer version of easy-purescript-nix:
+  # 1. Obtain the commit hash <rev> via `curl https://api.github.com/repos/justinwoo/easy-purescript-nix/commits/master`.
+  # 2. Obtain the sha256 hash <sha256> via `nix-prefetch-url --unpack https://github.com/justinwoo/easy-purescript-nix/archive/<rev>.tar.gz`.
+  # 3. Update the <rev> and <sha256> below.
+  pursPkgs = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "ee51a6d459b8fecfcb10f24ca9728e649c6a9e00";
+    sha256 = "1w4k6mcayyg6388na0cca5qx94sm99xn3na26x2w34jlyz3bwl3m";
+  }) {inherit pkgs;};
+in
+  pkgs.stdenv.mkDerivation {
+    name = "typedenv";
+    buildInputs = with pursPkgs; [
+      purs
+      spago
+      pulp
+      purs-tidy
+
+      pkgs.nodejs-16_x
+      pkgs.nodePackages.bower
+    ];
+  }
